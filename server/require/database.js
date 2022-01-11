590f10b2db2b3865d5a768b46fe6c42bdb3688ae
MD ISMAIL HOSEN <43248620+ismail61@users.noreply.github.com>
2022-01-11 11:57:58 +0600

    Update database.js

--- server/require/database.js
+++ server/require/database.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 
 //const url = 'mongodb://localhost:27017/library-management-system';
-//const url = 'mongodb+srv://root:<password61@>@database.dcg2q.mongodb.net/<test>?retryWrites=true&w=majority';
 mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
