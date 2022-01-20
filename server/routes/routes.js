9683e0fcef86a10b0a442cdc0066645a91491744
MD ISMAIL HOSEN <43248620+ismail61@users.noreply.github.com>
2022-01-21 01:37:35 +0600

    Delete server/routes directory

--- server/routes/routes.js
+++
@@ -1,8 +0,0 @@
-
-function routes(app) {
-    require('./auth/authRoutes.js')(app)
-    require('./admin/adminRoutes.js')(app)
-    require('./user/userRoutes.js')(app)
-}
-
-module.exports = routes
